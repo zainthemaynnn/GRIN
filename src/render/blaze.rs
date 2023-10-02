adbe6a5b79f5dddb53907c0480bedc23d53f4a5d
Zain Azam <zainyusufazam@gmail.com>
2023-10-02 00:31:56 -0400

    new blaze particles + configurable radius (untested!)

--- src/render/blaze.rs
+++ src/render/blaze.rs
@@ -38,65 +38,127 @@ impl Blaze {
 #[derive(Resource)]
 pub struct BlazeParticles {
     pub fire: Handle<EffectAsset>,
+    pub ring_wave: Handle<EffectAsset>,
 }
 
 pub fn create_blaze_effect_assets(mut commands: Commands, mut assets: ResMut<Assets<EffectAsset>>) {
-    let w = ExprWriter::new();
+    let fire = {
+        let w = ExprWriter::new();
 
-    let pos = SetPositionCircleModifier {
-        center: w.lit(Vec3::ZERO).expr(),
-        axis: w.lit(Vec3::Y).expr(),
-        radius: w.prop(Blaze::RADIUS_PROPERTY_TAG).expr(),
-        dimension: ShapeDimension::Volume,
+        let pos = SetPositionCircleModifier {
+            center: w.lit(Vec3::ZERO).expr(),
+            axis: w.lit(Vec3::Y).expr(),
+            radius: w.prop(Blaze::RADIUS_PROPERTY_TAG).expr(),
+            dimension: ShapeDimension::Volume,
+        };
+
+        let vel = SetVelocityModifier {
+            direction: w.lit(Vec3::Y).expr(),
+            speed: w.lit(1.0).uniform(w.lit(1.5)).expr(),
+        };
+
+        let age = SetAttributeModifier::new(Attribute::AGE, w.lit(0.0).uniform(w.lit(0.2)).expr());
+
+        let lifetime =
+            SetAttributeModifier::new(Attribute::LIFETIME, w.lit(0.8).uniform(w.lit(1.2)).expr());
+
+        let size = SizeOverLifetimeModifier {
+            gradient: distr::f32_map(10, &distr::linear, &distr::quad).fold(
+                Gradient::new(),
+                |mut grad, (i, t)| {
+                    grad.add_key(i, Vec2::splat(1.0 - t));
+                    grad
+                },
+            ),
+            screen_space_size: false,
+        };
+
+        let color = ColorOverLifetimeModifier {
+            gradient: distr::f32_map(10, &distr::linear, &distr::quad).fold(
+                Gradient::new(),
+                |mut grad, (i, t)| {
+                    grad.add_key(
+                        i,
+                        Vec4::new(1.0, 1.0, 0.0, 1.0).lerp(Vec4::new(1.0, 0.0, 0.0, 1.0), t),
+                    );
+                    grad
+                },
+            ),
+        };
+
+        let effect = EffectAsset::new(2048, Spawner::rate(16.0.into()), w.finish())
+            .with_name("blaze_particle")
+            .init(pos)
+            .init(vel)
+            .init(age)
+            .init(lifetime)
+            .render(color)
+            .render(size)
+            .render(BillboardModifier);
+
+        assets.add(effect)
     };
 
-    let vel = SetVelocityModifier {
-        direction: w.lit(Vec3::Y).expr(),
-        speed: w.lit(1.0).uniform(w.lit(1.5)).expr(),
+    let ring_wave = {
+        let w = ExprWriter::new();
+
+        let pos = SetPositionCircleModifier {
+            center: w.lit(Vec3::ZERO).expr(),
+            axis: w.lit(Vec3::Y).expr(),
+            radius: w.prop(Blaze::RADIUS_PROPERTY_TAG).expr(),
+            dimension: ShapeDimension::Volume,
+        };
+
+        let vel = SetVelocitySphereModifier {
+            speed: w.lit(1.0).uniform(w.lit(1.5)).expr(),
+            // TODO: REPLACE 1.0 WITH RADIUS_PROPERTY_TAG SOMEHOW
+            center: w
+                .lit(Vec3::new(0.0, -1.0 / 15.0_f32.to_radians().tan(), 0.0))
+                .expr(),
+        };
+
+        let lifetime = SetAttributeModifier::new(Attribute::LIFETIME, w.lit(0.5).expr());
+
+        let size = SizeOverLifetimeModifier {
+            gradient: distr::f32_map(10, &distr::linear, &distr::quad).fold(
+                Gradient::new(),
+                |mut grad, (i, t)| {
+                    grad.add_key(i, Vec2::splat((1.0 - t) * 0.2));
+                    grad
+                },
+            ),
+            screen_space_size: false,
+        };
+
+        let color = ColorOverLifetimeModifier {
+            gradient: distr::f32_map(10, &distr::linear, &distr::quad).fold(
+                Gradient::new(),
+                |mut grad, (i, t)| {
+                    grad.add_key(
+                        i,
+                        Vec4::new(1.0, 1.0, 0.0, 1.0).lerp(Vec4::new(1.0, 0.0, 0.0, 1.0), t),
+                    );
+                    grad
+                },
+            ),
+        };
+
+        let drag = LinearDragModifier::new(w.lit(5.0).expr());
+
+        let effect = EffectAsset::new(512, Spawner::once(128.0.into(), false), w.finish())
+            .with_name("ring_wave_particle")
+            .init(pos)
+            .init(vel)
+            .init(lifetime)
+            .update(drag)
+            .render(color)
+            .render(size)
+            .render(BillboardModifier);
+
+        assets.add(effect)
     };
 
-    let age = SetAttributeModifier::new(Attribute::AGE, w.lit(0.0).uniform(w.lit(0.2)).expr());
-
-    let lifetime =
-        SetAttributeModifier::new(Attribute::LIFETIME, w.lit(0.8).uniform(w.lit(1.2)).expr());
-
-    let size = SizeOverLifetimeModifier {
-        gradient: distr::f32_map(10, &distr::linear, &distr::quad).fold(
-            Gradient::new(),
-            |mut grad, (i, t)| {
-                grad.add_key(i, Vec2::splat(1.0 - t));
-                grad
-            },
-        ),
-        screen_space_size: false,
-    };
-
-    let color = ColorOverLifetimeModifier {
-        gradient: distr::f32_map(10, &distr::linear, &distr::quad).fold(
-            Gradient::new(),
-            |mut grad, (i, t)| {
-                grad.add_key(
-                    i,
-                    Vec4::new(1.0, 1.0, 0.0, 1.0).lerp(Vec4::new(1.0, 0.0, 0.0, 1.0), t),
-                );
-                grad
-            },
-        ),
-    };
-
-    let effect = EffectAsset::new(2048, Spawner::rate(16.0.into()), w.finish())
-        .with_name("blaze_particle")
-        .init(pos)
-        .init(vel)
-        .init(age)
-        .init(lifetime)
-        .render(color)
-        .render(size)
-        .render(BillboardModifier);
-
-    commands.insert_resource(BlazeParticles {
-        fire: assets.add(effect),
-    });
+    commands.insert_resource(BlazeParticles { fire, ring_wave });
 }
 
 pub fn spawn_blaze_particles(
@@ -113,9 +175,11 @@ pub fn spawn_blaze_particles(
         if let Some(spawner) = blaze.spawner.take() {
             effect = effect.with_spawner(spawner);
         }
-        commands
-            .entity(e_blaze)
-            .insert((effect, CompiledParticleEffect::default()));
+        commands.entity(e_blaze).insert((
+            effect,
+            CompiledParticleEffect::default()
+                .set_property(Blaze::RADIUS_PROPERTY_TAG, blaze.radius.into()),
+        ));
     }
 }
 
